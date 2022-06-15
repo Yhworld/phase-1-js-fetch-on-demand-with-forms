533a771806f345150c1c856fd32efe10a1bcd693
Yahya <yahyahussein670@gmail.com>
2022-06-15 15:07:40 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const init = () => {
+    const inputForm = document.querySelector('form')
+
+    inputForm.addEventListener('submit',(e) => {
+        e.preventDefault();
+    const input = document.querySelector('input#searchByID')
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then((res) => res.json())
+    .then((data=> {
+        const title = document.querySelector('#movieDetails h4')
+        const description = document.querySelector('#movieDetails p')
+
+        title.textContent = data.title;
+        description.textContent = data.summary;
+    })
+     )
+    })}
   
-}
+
 
 document.addEventListener('DOMContentLoaded', init);
